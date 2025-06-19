3a1207d6210eaf6708ebecbe55d5b89f60f82917
Corvo <rodrigo-f-p@hotmail.com>
2025-06-19 14:36:27 -0300

    Update player.py

--- src/player.py
+++ src/player.py
@@ -1,6 +1,6 @@
 class Player:
 
-    def __init__(self, name, cards)
+    def __init__(self, name, cards):
         self.name = name
         self.dices = [4, 6, 8, 10, 12, 20]
         self.cards = cards
