4c7c7eefc69fb923fd6330f3ed17fbc350c3afce
Corvo <rodrigo-f-p@hotmail.com>
2025-06-19 14:40:50 -0300

    Update player.py

--- src/player.py
+++ src/player.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Player:
 
     def __init__(self, name, cards):
@@ -5,3 +7,7 @@ class Player:
         self.dices = [4, 6, 8, 10, 12, 20]
         self.cards = cards
         self.main = None
+
+    def roll(self, dice):
+        return self.dices.pop(dice)
+        
