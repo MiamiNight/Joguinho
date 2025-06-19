8a63bd0a205457ded9d01870252e8e530d7dfb0f
Corvo <rodrigo-f-p@hotmail.com>
2025-06-19 14:25:38 -0300

    Update cards.py
    Create basic monster class

--- src/cards.py
+++ src/cards.py
@@ -1,1 +1,10 @@
-print('hello world')
+# Basic Monster class:
+class Monster():
+
+    def __init__(self, name, fragility, weak_point):
+        self.name = name
+        self.fragility = fragility
+        self.weak_point = weak_point
+      
+    def skills(self):
+        pass # For future implementation
