e87bb26ad7575a180c9b7ac187800e3a5c033fb2
Corvo <rodrigo-f-p@hotmail.com>
2025-06-19 15:09:07 -0300

    Update monster.py

--- src/monster.py
+++ src/monster.py
@@ -1,10 +1,11 @@
 # Basic Monster class:
 class Monster():
 
-    def __init__(self, name, fragility, weak_point):
+    def __init__(self, name, fragility, weak_point, img):
         self.name = name
         self.fragility = fragility
         self.weak_point = weak_point
+        self.img = img
       
     def skills(self):
         pass # For future implementation
